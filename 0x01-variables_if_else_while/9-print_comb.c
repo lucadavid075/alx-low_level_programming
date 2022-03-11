7c06a90e87f45c03dfd75028cd4af51348c857a4
lucadavid075 <daiveedlucas049@gmail.com>
2022-03-11 06:50:00 -0800

    commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,11 @@ int main(void)
 	for (number = 0; number <= 9; number++)
 	{
 		putchar(number + '0');
-	
 		if (number < 9)
+		{
 			putchar(',');
-			
-		putchar(' ');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 
