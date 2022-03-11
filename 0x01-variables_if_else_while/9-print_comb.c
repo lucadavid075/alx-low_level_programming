2613bcc14dc7b06e2143956df802dcaa80650cad
lucadavid075 <daiveedlucas049@gmail.com>
2022-03-11 04:53:38 -0800

    fixed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,14 @@ int main(void)
 	int number;
 
 	for (number = 0; number <= 9; number++)
+	{
 		putchar(number + '0');
-
-	if (number < 9)
-		putchar(',');
-
-	putchar(' ');
-
+	
+		if (number < 9)
+			putchar(',');
+			
+		putchar(' ');
+	}
 	putchar('\n');
 
 	return (0);
