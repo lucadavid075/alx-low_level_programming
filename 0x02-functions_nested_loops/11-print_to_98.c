6223bca702e96ccebacd5ba3b4b91916d9a8643e
lucadavid075 <daiveedlucas049@gmail.com>
2022-03-15 16:26:38 -0700

    fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,9 +9,9 @@
 
 void print_to_98(int n)
 {
-	if  (n > 98)
+	if  (n <= 98)
 	{
-		for (; n > 98; n--)
+		for (; n <= 98; n++)
 		{
 			printf("%d, ", n);
 
@@ -19,18 +19,18 @@ void print_to_98(int n)
 				continue;
 			printf(", ");
 		}
-		printf('\n');
+		printf("\n");
 	}
 	else
 	{
-		for (; n < 98; n++)
+		for (; n >= 98; n--)
 		{
-			printf("%d, " n);
+			printf("%d, ", n);
 
 			if (n == 98)
 				continue;
 			printf(", ");
 		}
-		printf('\n');
+		printf("\n");
 	}
 }
