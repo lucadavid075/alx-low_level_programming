364ca3498d1c99de71fecb8085a1cd62445b96aa
lucadavid075 <daiveedlucas049@gmail.com>
2022-03-15 12:57:02 -0700

    Added 11-print_to_98.c file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Entry point
+ * Description: prints all natural numbers from n to 98
+ * @n: Number to start
+ * Return: 0 or 1
+ */
+
+void print_to_last(int n)
+{
+	while (n < 98)
+	{
+		printf("%i, ", n);
+		n--;
+	}
+	printf("98");
+	putchar('\n');
+}
