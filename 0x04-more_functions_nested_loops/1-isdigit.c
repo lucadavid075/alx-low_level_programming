2cffad2e87782625f8bec24939945b60cd8dc5c6
lucadavid075 <daiveedlucas049@gmail.com>
2022-03-17 06:25:14 -0700

    Added 1-isdigit.c file

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,11 +2,12 @@
 
 /**
  * _isdigit - checks for a digit (0 throught 9)
- * @c: An input alphabet to be checked
- * Return: 1 if c is a digit and 0 otherwise
+ * @c: An input alphabet to be checke
+ * Return: 1 if c is a digit or  0 otherwise
  */
 
 int _isdigit(int c)
 {
+
 	return (c >= 48 && c <= 57);
 }
