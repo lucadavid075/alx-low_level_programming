ddfbb05e541f82934d48bb9726f060357a1d3882
lucadavid075 <daiveedlucas049@gmail.com>
2022-03-21 16:43:37 -0700

    Fixed 100-atoi.c file

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
 int _atoi(char *s)
 {
 	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
-	
+
 	while (*(s + count) != '\0')
 	{
 		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
