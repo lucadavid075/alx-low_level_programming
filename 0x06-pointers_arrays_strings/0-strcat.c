c907443813191e7b9946109e66ed98b92e2e0de4
lucadavid075 <daiveedlucas049@gmail.com>
2022-03-24 18:47:28 -0700

    Another update on the directory

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int a = -1, i;
 
-	i = 0;
-	j = 0;
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-	while (dest[i] != '\0')
-	{
+	do {
+		a++;
+		dest[i] = src[a];
 		i++;
-	}
+	} while (src[a] != '\0');
 
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++
-	}
-	dest[i] = '\0';
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,20 +8,16 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int count = 0, count2 = 0;
+	int dest_len, i;
 
-	while (*(dest + count) != '\0')
-	{
-		count++;
-	}
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+		;
 
-	while (count2 < n)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count++;
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+
+	/*should end with a end of string char*/
+	dest[dest_len + i] = '\0';
+
 	return (dest);
 }
