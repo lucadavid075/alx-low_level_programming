4d86f93e7c2ad27ed3509ad27d431f46d5baed06
lucadavid075 <daiveedlucas049@gmail.com>
2022-04-05 00:29:09 -0700

    Added betty styling for 2-str_concat.c file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,12 +11,12 @@
 
 int _strlen(char *s)
 {
-int size = 0;
-for (; s[size] != '\0'; size++)
-;
-return (size);
-}
+	int size = 0;
 
+	for (; s[size] != '\0'; size++)
+		;
+	return (size);
+}
 /**
  * *str_concat - concatenates two strings
  * @s1: string 1
@@ -26,27 +26,27 @@ return (size);
 
 char *str_concat(char *s1, char *s2)
 {
-int size1, size2, i;
-char *m;
+	int size1, size2, i;
+	char *m;
 
-if (s1 == NULL)
-	s1 = "\0";
-if (s2 == NULL)
-	s2 = "\0";
+	if (s1 == NULL)
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = "\0";
 
-size1 = _strlen(s1);
-size2 = _strlen(s2);
-m = malloc((size1 + size2) *sizeof(char) + 1);
-if (m == 0)
-	return (0);
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+	m = malloc((size1 + size2) * sizeof(char) + 1);
+	if (m == 0)
+		return (0);
 
-for (i = 0; i <= size1 + size2; i++)
-{
-	if (i < size1)
-		m[i] = s1[i];
-	else
-		m[i] = s2[i - size1];
-}
-m[i] = '\0';
-return (m);
+	for (i = 0; i <= size1 + size2; i++)
+	{
+		if (i < size1)
+			m[i] = s1[i];
+		else
+			m[i] = s2[i - size1];
+	}
+	m[i] = '\0';
+	return (m);
 }
