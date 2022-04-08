42814b94b98593ecbbd383e23277021c2f15d9fc
Luke David <daiveedlucas049@gmail.com>
2022-04-08 04:25:33 +0100

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,7 +6,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
 int _putchar(char c);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
 void _puts(char *str);
+
+int _strlen(char *);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int onlyNumbers(char *c);
+void multiply(char* s1, char* s2);
