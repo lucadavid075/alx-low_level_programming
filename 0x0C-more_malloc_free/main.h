e83e73b4b870ad9eff179e65063aa371827da800
Luke David <daiveedlucas049@gmail.com>
2022-04-10 07:41:19 +0100

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,10 +7,10 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-int is_digit(char *s)
-int _strlen(char *s)
-void errors(void)
-int main(int argc, char *argv[])
+int is_digit(char *s);
+int _strlen(char *s);
+void errors(void);
+int main(int argc, char *argv[]);
   
 int _putchar(char c);
 int _atoi(const char *s);
