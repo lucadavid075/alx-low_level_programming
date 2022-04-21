5daeb67b3f512fff8c86920da44d8d94d424829a
lucadavid075 <daiveedlucas049@gmail.com>
2022-04-21 16:57:04 -0700

    [Feature] Add function that prints *el of list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to head of singly linked list
+ *
+ * Return: number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int i;
+
+	for (i = 0; h; i++)
+	{
+		if (h->str != 0)
+			printf("[%d] %s\n", h->len, h->str)
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+	}
+	return (i);
+}
