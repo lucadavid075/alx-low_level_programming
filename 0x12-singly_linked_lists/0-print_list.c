191c7d15e9b0d4550fc470fe87d8a393a65dc557
lucadavid075 <daiveedlucas049@gmail.com>
2022-04-21 18:11:01 -0700

    [Fix] Requirement fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,12 +8,12 @@
  */
 size_t print_list(const list_t *h)
 {
-	unsigned int i;
+	size_t i;
 
 	for (i = 0; h; i++)
 	{
 		if (h->str != 0)
-			printf("[%d] %s\n", h->len, h->str)
+			printf("[%d] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
 		h = h->next;
