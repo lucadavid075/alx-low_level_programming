14693c1f56895779349ea88c94aa78ff834f4696
Luke David <daiveedlucas049@gmail.com>
2022-04-22 03:21:52 +0100

    [fix](I'm tired)..

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,7 @@
 #include "lists.h"
-
 /**
  * print_list - prints all the elements of a list_t list.
- * @h: ponter to head of linked list.
- 
+ * @h: singly linked list.
  * Return: number of elements in the list.
  */
 
