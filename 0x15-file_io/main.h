acf61d5acf7bf3dfedf49fbd723ebe000845efe3
Luke David <daiveedlucas049@gmail.com>
2022-05-02 14:31:23 +0000

    [UPDATE] header file update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef _MAIN_
+#define _MAIN_
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -7,12 +7,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <elf.h>
 
-int _putchar(char c);
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
 
 #endif
