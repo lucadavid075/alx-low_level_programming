9f8056bd1e690a3304e5a13ce5b889f2fc807110
lucadavid075 <daiveedlucas049@gmail.com>
2022-05-02 05:39:36 -0700

    [FIX] betty error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,7 @@
  * standard output.
  * @filename: file.
  * @letters: number of letters it should read and print.
- * 
+ *
  * Return: actual number of letters it could read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
